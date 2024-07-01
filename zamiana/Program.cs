6685217c6498bc63d585ec32a6c4c115cf6cd550
JA <JA@DESKTOP-NF8JU3C>
2024-07-01 15:16:29 +0200

    Add project files.

---
+++ zamiana/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Text.RegularExpressions;
+
+String filename = Console.ReadLine();
+
+String line = "";
+try
+{
+    StreamReader reader = new StreamReader(filename + ".txt");
+    StreamWriter writer = new StreamWriter(filename + "_changed.txt");
+
+    line = reader.ReadLine();
+
+    while(line != null)
+    {
+        String newLIne = Regex.Replace(line, "praca", "job");
+
+        writer.WriteLine(newLIne);
+        line = reader.ReadLine();
+    }
+    writer.Flush();
+}
+catch(Exception e)
+{
+    Console.WriteLine(e);
+}
